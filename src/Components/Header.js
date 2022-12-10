f039f06e0830c71a5ff0049b1e32e15bd507644c
towhid7667 <towhidulhq@gmail.com>
2022-12-10 12:24:45 +0600

    Starting commit

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './../img/MuzicLogo.png'
+const Header = () => {
+    return (
+        <div>
+            <div className="logo">
+                <img src="" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Header;
