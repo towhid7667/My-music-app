7d1885c1bdc84af6e4dd53a9ddf74582e650a35e
towhid7667 <towhidulhq@gmail.com>
2022-12-10 17:19:10 +0600

    banner done

--- src/Components/MainBanner.js
+++ src/Components/MainBanner.js
@@ -7,10 +7,12 @@ import p4 from './../img/p 4.png'
 const MainBanner = () => {
     return (
         <div className='bg-[#232A4E] flex items-center justify-around py-8 h-screen relative rounded-b-3xl'>
-            <div>
-
+            <div className='w-1/2'>
+            <h1 className='text-7xl text-gray-400'>Experience The</h1>
+            <h1 className='text-7xl font-bold'>Best quality Music</h1>
+            <p className='w-3/4 my-5 text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero laboriosam error ab rem, dicta cupiditate voluptatem atque deleniti quos ducimus!</p>
             </div>
-            <div>
+            <div className='px-32'>
                 <img src={p1} alt=""  className=' absolute -top-10 right-20 scale-75'/>
                 <img src={p2} alt="" className='absolute bottom-[5.4rem] right-40 scale-[.71] '/>
                 <img src={p3} alt="" className='absolute -bottom-[3.5rem] right-[14.75rem] scale-[.25]'/>
